b250d7dc233711de04d0842466e415c0f0644fbd
pierreq <pierre@eventtia.com>
2021-07-31 12:53:58 -0400

    ideas para implementar

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   get 'about_us', to: 'home#about_us'
   get 'dashboard', to: 'home#dashboard'
   get 'users/:id', to: 'users#update_status', as: :update_status
-  match '/users/:id', to: 'users#destroy', as: :destroy_user, via: :delete
+  delete '/users/:id', to: 'users#destroy', as: :destroy_user
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
